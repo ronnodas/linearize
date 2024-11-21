5cd3ebab5b064c39af5998a45118229144399454
Julian Orth <ju.orth@gmail.com>
2024-11-21 13:13:43 +0100

    Fix static_copy_map macro

--- linearize-tests/src/tests/macro.rs
+++ linearize-tests/src/tests/macro.rs
@@ -71,3 +71,35 @@ fn constants_of_type_given() {
     assert_eq!(*map[L::False], 0);
     assert_eq!(*map[L::True], 1);
 }
+
+mod copy_macro {
+    use linearize::{static_copy_map, Linearize, StaticCopyMap};
+
+    #[test]
+    fn test() {
+        let _: StaticCopyMap<bool, u8> = static_copy_map! {
+            false => 1,
+            _ => 1,
+        };
+    }
+
+    #[test]
+    #[cfg(more_const_functions)]
+    fn test_constant() {
+        #[derive(Linearize)]
+        #[linearize(const)]
+        enum L {
+            False,
+            True,
+        }
+        let _: StaticCopyMap<L, u8> = static_copy_map! {
+            of type L:
+            _ => 1,
+        };
+        let _: StaticCopyMap<L, u8> = static_copy_map! {
+            constants of type L:
+            L::True => 1,
+            L::False => 1,
+        };
+    }
+}
